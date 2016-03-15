d0db71a7caaa3614ffda4ad779ffe8968c4d4625
root <root@raspberrypi.(none)>
2016-03-15 09:35:37 +0000

    Моргание лампочки на 0.2 с

--- py/test/test1.py
+++ py/test/test1.py
@@ -1,22 +1,42 @@
+#!/usr/bin/python
+
+#http://www.raspberrypi-spy.co.uk/2012/06/simple-guide-to-the-rpi-gpio-header-and-pins/#prettyPhoto
 import RPi.GPIO as GPIO
+import time as time
  
-# use P1 header pin numbering convention
-GPIO.setmode(GPIO.BOARD)
+#GPIO.RPI_INFO
+chl = []
+a = 1
+while a < 31:
+  #print a
+  a = a+1
+  chl.append(a)
+
+#ch = 27
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BOARD)
+print chl
  
-# Set up the GPIO channels - one input and one output
-GPIO.setup(11, GPIO.IN)
-GPIO.setup(12, GPIO.OUT)
- 
-# Input from pin 11
-input_value = GPIO.input(11)
-print input_value;
- 
-# Output to pin 12
-#GPIO.output(12, GPIO.HIGH)
- 
-# The same script as above but using BCM GPIO 00..nn numbers
-#GPIO.setmode(GPIO.BCM)
-#GPIO.setup(17, GPIO.IN)
-#GPIO.setup(18, GPIO.OUT)
-#input_value = GPIO.input(17)
-#GPIO.output(18, GPIO.HIGH)
+bad = []
+good = []
+for ch in chl:
+    try:
+        GPIO.setup(ch, GPIO.OUT)
+        good.append(ch)
+    except:
+        bad.append(ch)
+
+print "Bad : " + str(bad)
+print "Good: " + str(good)
+
+GPIO.cleanup()
+ch = 2
+
+while 1: 
+	print 111
+	GPIO.setup(ch, GPIO.HIGH);
+	time.sleep(0.2);
+	print 000
+	GPIO.setup(ch, GPIO.LOW);
+	time.sleep(0.2);
